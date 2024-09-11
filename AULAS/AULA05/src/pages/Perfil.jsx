99940c871621c72c3da328a17141bd32de329e3a
Ingrid Maria <ingridmr857@gmail.com>
2024-09-10 21:56:06 -0300

    Finalização de conteudo Aulas do primeiro bimestre

---
+++ AULAS/AULA05/src/pages/Perfil.jsx
@@ -0,0 +1,10 @@
+function Perfil(props){
+    return (
+        <>
+        <h1>Perfil do Usuario</h1>
+        <Menu navegaPara = {props.navegaPara}/>
+        </>
+    )
+}
+
+export default Perfil;
