95a7cfa35a1d23a1de1761a374762ef7fc805b5b
Ingrid Maria <ingridmr857@gmail.com>
2024-09-24 21:47:00 -0300

    Aula06 sobre Routes

---
+++ AULAS/AULA06/src/App.jsx
@@ -0,0 +1,23 @@
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import Login from './pages/Login';
+import Perfil from './pages/Perfil';
+import Erro404 from './pages/Erro404';
+import Home from './pages/Home';
+import Leiaute from './pages/Leiaute';
+ 
+function App(){
+  return(
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login/>}/>
+        <Route element={<Leiaute/>}>
+        <Route path="/home" element={<Home/>}/>
+        <Route path="/perfil" element={<Perfil/>}/>
+        </Route>
+        <Route path="*" element={<Erro404/>}/>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App;
