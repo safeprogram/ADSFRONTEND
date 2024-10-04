8539192a92684266c2cfa92e7de474b3d393bcc1
Ingrid Maria <ingridmr857@gmail.com>
2024-10-04 20:25:29 -0300

    Finalizando Aula07

--- AULAS/AULA07/src/pages/Home.jsx
+++ AULAS/AULA07/src/pages/Home.jsx
@@ -1,6 +1,16 @@
+import { useEffect, useState} from "react";
 import Menu from "../components/Menu";
 
 function Home() {
+  const [carregando, setCarregando] = useState(true);
+
+  useEffect(() => {
+    const timeout = setTimeout(()=>{
+      setCarregando(false)
+    }, 3000);
+    return() => clearTimeout(timeout);
+  }, []);
+
   return (
     <>
       <header>
@@ -10,6 +20,33 @@ function Home() {
         <Menu />
         <section>
           <h2>Página Inicial</h2>
+          {carregando ? <h3>Aguarde...</h3> 
+          : <div className="card-grid">
+            <article>
+              <h3>Mural de Aviso</h3>
+              <ul>
+                <li></li>
+              </ul>
+            </article>
+            <article>
+              <h3>Agenda Acadêmica</h3>
+              <ul>
+                <li></li>
+              </ul>
+            </article>
+            <article>
+              <h3>Histórico de Notas</h3>
+              <ul>
+                <li></li>
+              </ul>
+            </article>
+            <article>
+              <h3>Histórico de Faltas</h3>
+              <ul>
+                <li></li>
+              </ul>
+            </article>
+          </div>}
         </section>
       </main>
     </>
